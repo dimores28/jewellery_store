7ffb5b489d0390fdbe67306953ef993cb5a2c098
Dmytro <dmytroivanovichn@gmail.com>
2024-03-15 20:32:39 +0200

    create special-offer section

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,25 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+let player = document.querySelector('#specialOfferPlayer');
+let playBtnWrap = document.querySelector('.video__btn-wrp');
+let playBtn = document.querySelector('.video__btn');
+
+playBtn?.addEventListener('click', function() {
+    player.play();
+});
+
+playBtn?.addEventListener('click', function() {
+    player.play();
+});
+
+player?.addEventListener('play', function() {
+    playBtnWrap.style.display = 'none';
+    player.setAttribute("controls", "controls");
+})
+
+player?.addEventListener('pause', function() {
+    playBtnWrap.style.display = 'flex';
+})
